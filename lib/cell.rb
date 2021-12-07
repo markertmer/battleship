c4260e49b067922368798317d6d0c68f91e3b6e7
Sulaiman Birashk <sully.birashk@gmail.com>
2021-12-06 19:23:10 -0700

    Feat: Coordinate Function, Test: Coordinate

--- lib/cell.rb
+++ lib/cell.rb
@@ -2,9 +2,13 @@ require './lib/ship'
 
 class Cell
   attr_reader :location
-  
+
   def initialize(location)
     @location = location
+  end
+
+  def coordinate
+    return @location
   end
 
 end
--- spec/cell_sepc.rb
+++ spec/cell_sepc.rb
@@ -8,4 +8,11 @@ RSpec.describe "Cell" do
     cell = Cell.new("B4")
     expect(cell).to be_instance_of(Cell)
   end
+
+  it "Checks Location of Argunment" do
+    cell = Cell.new("B4")
+    #binding.pry
+    expect(cell.coordinate).to eq("B4")
+  end
+
 end
