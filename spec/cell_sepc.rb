f8f76533967d9f7bd241173919330fdab259e27f
Mark Ertmer <mark.ertmer@gmail.com>
2021-12-07 18:51:38 -0700

    feat: add & test render

--- spec/cell_sepc.rb
+++ spec/cell_sepc.rb
@@ -39,6 +39,7 @@ RSpec.describe "Cell" do
     cruiser = Ship.new("Cruiser", 3)
     #binding.pry
     cell.place_ship(cruiser)
+    #binding.pry
     expect(cell.fired_upon?).to be(false)
   end
 
@@ -63,16 +64,51 @@ RSpec.describe "Cell" do
     expect(cell.fired_upon?).to be(true)
   end
 
-  it "exists" do
+  it 'exists' do
     cell_1 = Cell.new("B4")
     #binding.pry
     expect(cell_1).to be_instance_of(Cell)
   end
 
-  it "Renders a Cell" do
+  it 'Renders as empty or miss' do
     cell_1 = Cell.new("B4")
-    binding.pry
+    expect(cell_1.empty?).to be(true)
+    expect(cell_1.fired_upon?).to be(false)
     expect(cell_1.render).to eq(".")
+    cell_1.fire_upon
+    expect(cell_1.render).to eq("M")
+  end
+
+  it 'ships can be hidden or revealed' do
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+    cell_2.place_ship(cruiser)
+
+    expect(cell_2.render). to eq(".")
+    expect(cell_2.render(true)).to eq("S")
+  end
+
+  it 'renders a hit' do
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+    cell_2.place_ship(cruiser)
+    cell_2.fire_upon
+    expect(cell_2.render).to eq("H")
+  end
+
+  it 'sinks a ship' do
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+    cell_2.place_ship(cruiser)
+    cell_2.fire_upon
+
+    expect(cruiser.sunk?).to be false
+
+    cruiser.hit
+    cruiser.hit
+
+    expect(cruiser.sunk?).to be true
+    expect(cell_2.render).to eq("X")
   end
 
 end
