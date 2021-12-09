68848ecd1214fb3f7d30122b28b3500906b85351
Sulaiman Birashk <sully.birashk@gmail.com>
2021-12-09 14:25:47 -0700

    Feat Comp Place in progress

--- lib/runner_class.rb
+++ lib/runner_class.rb
@@ -6,11 +6,14 @@ require 'pry'
 class BattleshipRunner
 
   def initialize
-    @board = Board.new
+    @player_board = Board.new
+    @computer_board = Board.new
+
   end
 
   def start
     main_menu
+    comp_place
   end
 
   def main_menu
@@ -19,7 +22,30 @@ class BattleshipRunner
     if input == "q"
       exit
     end
-    #binding.pry
   end
+
+  def comp_place
+    @cruiser = Ship.new("cruiser", 3)
+    @submarine = Ship.new("submarine", 2)
+    x = cell_random(3)
+    if @computer_board.valid_placement?(@cruiser, x)
+      @computer_board.place(@cruiser, x)
+    else
+      x = cell_random(3)
+      
+    end
+
+    binding.pry
+
+    # until @computer_board.valid_placement?(@cruiser, x)
+    #  x
+    end
+  end
+
+    def cell_random(num)
+      random_cells = @computer_board.cells.keys.sample(num)
+    end
+
+
 
 end
