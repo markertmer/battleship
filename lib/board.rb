65526f3bbbc6279caf5a6c813acadb353d34b1b0
Sulaiman Birashk <sully.birashk@gmail.com>
2021-12-13 13:53:05 -0700

    Feat: Customizable Board Size

--- lib/board.rb
+++ lib/board.rb
@@ -1,25 +1,41 @@
 class Board
   attr_reader :cells
 
-  def initialize
-    @cells = {
-      "A1" => Cell.new("A1"),
-      "A2" => Cell.new("A2"),
-      "A3" => Cell.new("A3"),
-      "A4" => Cell.new("A4"),
-      "B1" => Cell.new("B1"),
-      "B2" => Cell.new("B2"),
-      "B3" => Cell.new("B3"),
-      "B4" => Cell.new("B4"),
-      "C1" => Cell.new("C1"),
-      "C2" => Cell.new("C2"),
-      "C3" => Cell.new("C3"),
-      "C4" => Cell.new("C4"),
-      "D1" => Cell.new("D1"),
-      "D2" => Cell.new("D2"),
-      "D3" => Cell.new("D3"),
-      "D4" => Cell.new("D4"),
-    }
+  def initialize(size)
+    key = "1A2B3C4D5E6F7G8H9I"
+    numbers = ("1".."#{size}").to_a
+    letters = []
+    coords = []
+    numbers.each do |num|
+      index = key.index(num)
+      let = key[index + 1]
+      numbers.each do |num2|
+        coords << let + num2
+      end
+    end
+    @cells = {}
+    coords.each do |coord|
+      @cells[coord] = Cell.new(coord)
+    end
+    #binding.pry
+    # @cells = {
+      # "A1" => Cell.new("A1"),
+      # "A2" => Cell.new("A2"),
+      # "A3" => Cell.new("A3"),
+      # "A4" => Cell.new("A4"),
+      # "B1" => Cell.new("B1"),
+      # "B2" => Cell.new("B2"),
+      # "B3" => Cell.new("B3"),
+      # "B4" => Cell.new("B4"),
+      # "C1" => Cell.new("C1"),
+      # "C2" => Cell.new("C2"),
+      # "C3" => Cell.new("C3"),
+      # "C4" => Cell.new("C4"),
+      # "D1" => Cell.new("D1"),
+      # "D2" => Cell.new("D2"),
+      # "D3" => Cell.new("D3"),
+      # "D4" => Cell.new("D4"),
+    # }
   end
 
   def valid_coordinate?(coordinate)
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,23 +5,23 @@ require 'pry'
 
 RSpec.describe "Cell" do
   it "exists" do
-    board = Board.new
+    board = Board.new(4)
     expect(board).to be_instance_of(Board)
   end
 
   it "has coordinates" do
-    board = Board.new
+    board = Board.new(4)
     expect(board.cells.keys).to eq(["A1","A2","A3","A4","B1","B2","B3","B4","C1","C2","C3","C4","D1","D2","D3","D4"])
   end
 
   it "validates the coordinate" do
-    board = Board.new
+    board = Board.new(4)
     expect(board.valid_coordinate?("A1")).to be(true)
     expect(board.valid_coordinate?("E1")).to be(false)
   end
 
   it "maps ships to placement" do
-    board = Board.new
+    board = Board.new(4)
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
     expect(board.valid_placement?(cruiser, ["A1", "A2"])).to be(false)
@@ -33,14 +33,14 @@ RSpec.describe "Cell" do
   end
 
   it "validates consecutive coordinates" do
-    board = Board.new
+    board = Board.new(4)
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
     expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to be(false)
   end
 
   it "coordinates can’t be diagonal" do
-    board = Board.new
+    board = Board.new(4)
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
     expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to be(false)
@@ -48,7 +48,7 @@ RSpec.describe "Cell" do
   end
 
   it "places a ship" do
-    board = Board.new
+    board = Board.new(4)
     cruiser = Ship.new("Cruiser", 3)
     board.place(cruiser, ["A1", "A2", "A3"])
 
@@ -63,7 +63,7 @@ RSpec.describe "Cell" do
   end
 
   it "does not allow overlap" do
-    board = Board.new
+    board = Board.new(4)
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
     board.place(cruiser, ["A1", "A2", "A3"])
@@ -73,11 +73,16 @@ RSpec.describe "Cell" do
   end
 
   it "renders the board" do
-    board = Board.new
+    board = Board.new(4)
     cruiser = Ship.new("Cruiser", 3)
     board.place(cruiser, ["A1", "A2", "A3"])
 
     expect(board.render).to eq("  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n")
     expect(board.render(true)).to eq("  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n")
+  end
+
+  it "Chooses a board size" do
+    board = Board.new(6)
+    expect(board.cells.keys).to eq(["A1","A2","A3","A4","A5","A6","B1","B2","B3","B4","B5","B6","C1","C2","C3","C4","C5","C6","D1","D2","D3","D4","D5","D6","E1","E2","E3","E4","E5","E6","F1","F2","F3","F4","F5","F6"])
   end
 end
