f18ad2e52f1dcee996aac9be8a72615cb27b533e
Mark Ertmer <mark.ertmer@gmail.com>
2021-12-15 16:12:51 -0700

    feat: Brain.hot_coords

--- lib/comp_brain.rb
+++ lib/comp_brain.rb
@@ -26,4 +26,13 @@ class CompBrain
     end
     return adjacent_cells
   end
+
+  def hot_coords(board)
+    all_adj_coord = adj_coords(board)
+    hot_coords = all_adj_coord.reject do |coord|
+      !board.valid_coordinate?(coord) ||
+      board.cells[coord].render != "."
+    end
+    return hot_coords
+  end
 end
